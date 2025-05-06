8bd313d99dd040073ccb058e82c2b44bc7f88feb
Romeo Extember <2201101589@student.buksu.edu.ph>
2025-05-06 22:51:08 +0800

    Color Fixed

--- resources/views/tenant/Profile/Profile.blade.php
+++ resources/views/tenant/Profile/Profile.blade.php
@@ -556,7 +556,7 @@
                                         </div>
                                         
                                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                            <button type="submit" class="btn btn-primary" id="updatePasswordBtn" {{ !$isPremium && !$isUltimate ? 'disabled' : '' }}>
+                                            <button type="submit" class="btn btn-primary" id="updatePasswordBtn" {{ !$isPremium && !$isUltimate ? 'disabled' : '' }} style="color: white;">
                                                 <i class="fas fa-key me-2"></i>Update Password
                                             </button>
                                         </div>
